70a4efa630cda4a442fc5e9d33d6fdc1a57f518d
Muhammad Fajri <160698852+Fajri135@users.noreply.github.com>
2024-03-19 12:40:25 +0700

    189_LuasPersegi
    Membuat program untuk menentukan luas persegi beserta dengan prosedurnya

---
+++ 189_LuasPersegi/189_LuasPersegi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int sisi;
+
+void inputdata() {
+    cout << "Masukan Nilai sisi : ";
+    cin >> sisi;
+}
+
+int hitungluas() {
+    return sisi * sisi;
+}
+void display() {
+    cout << "Luas Persegi : " << hitungluas() << endl;
+}
+
+int main()
+{
+    inputdata();
+    display();
+}
